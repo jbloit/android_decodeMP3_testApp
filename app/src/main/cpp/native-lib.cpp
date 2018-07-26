69edc24fd06146230a9c69f590909d6bcd127cf5
Julien Bloit <julien.bloit@gmail.com>
2018-07-26 15:08:22 +0200

    loads ffmpeg and displays version information

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,6 @@
 #include <jni.h>
 #include <string>
-#include "DecodeMp3.h"
+
 
 extern "C"
 {
@@ -12,30 +12,4 @@ Java_com_jbloit_decodemp3_MainActivity_stringFromJNI(
     return env->NewStringUTF(hello.c_str());
 }
 
-
-JNIEXPORT jstring JNICALL
-Java_com_jbloit_decodemp3_MainActivity_decodeMp3(
-        JNIEnv *env,
-        jobject /* this */,
-        jstring filename) {
-
-    DecodeMp3 *decoder = new(std::nothrow) DecodeMp3();
-
-
-    // convert Java string to UTF-8
-    jboolean isCopy;
-    const char *utf8 = env->GetStringUTFChars(filename, &isCopy);
-    assert(NULL != utf8);
-    std::string str = std::string(utf8);
-    decoder->loadAudioFileSimple(str);
-    // see https://stackoverflow.com/questions/5859673/shoul
-    // d-you-call-releasestringutfchars-if-getstringutfchars-returned-a-copy
-    env->ReleaseStringUTFChars(filename, utf8);
-
-
-    std::string hello = "Hello from C++";
-    DecodeMp3 *decoder = new(std::nothrow) DecodeMp3();
-
-    return env->NewStringUTF(hello.c_str());
-}
 }
--- app/src/main/java/com/jbloit/decodemp3/MainActivity.kt
+++ app/src/main/java/com/jbloit/decodemp3/MainActivity.kt
@@ -7,12 +7,20 @@ import android.os.Bundle
 import android.os.Environment
 import android.support.v4.app.ActivityCompat
 import android.util.Log
+import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler
+import com.github.hiteshsondhi88.libffmpeg.FFmpeg
+import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MAIN"
+
+    var ffmpeg : FFmpeg? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,15 +31,73 @@ class MainActivity : AppCompatActivity() {
         val testDir = File(baseDir.absolutePath + "/test")
         val wavFile = File(testDir, "sinusoid.wav")
 
-        val fileWriter = WaveFileWriter(wavFile)
-        fileWriter.write(audioBuffer, 0, audioBuffer.size)
-        fileWriter.close()
-        Log.d(TAG, "WROTE FILE")
-
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
+
+
+        ffmpeg = FFmpeg.getInstance(this)
+        try {
+            ffmpeg?.loadBinary(object : LoadBinaryResponseHandler(){
+
+                override fun onStart() {
+                    super.onStart()
+                }
+
+                override fun onFailure() {
+                    super.onFailure()
+                }
+
+                override fun onSuccess() {
+                    super.onSuccess()
+                }
+
+                override fun onFinish() {
+                    super.onFinish()
+                }
+            })
+        } catch (e: FFmpegNotSupportedException) {
+            Log.d(TAG, "load ffmpeg failed")
+            e.printStackTrace()
+
+        }
+
+        runFFmpegCommand()
     }
 
+
+    fun runFFmpegCommand(){
+        val command = arrayOf("-version")
+        try {
+            ffmpeg?.execute(command, object : ExecuteBinaryResponseHandler(){
+                override fun onStart() {
+                    super.onStart()
+                }
+
+                override fun onProgress(message: String?) {
+                    super.onProgress(message)
+                    Log.d(TAG, "progress : ${message}")
+                }
+
+                override fun onFailure(message: String?) {
+                    super.onFailure(message)
+                    Log.d(TAG, "failure : ${message}")
+                }
+
+                override fun onSuccess(message: String?) {
+                    super.onSuccess(message)
+                    Log.d(TAG, "success : ${message}")
+                    sample_text.text = message
+                }
+
+                override fun onFinish() {
+                    super.onFinish()
+                }
+            })
+        } catch (e: FFmpegCommandAlreadyRunningException) {
+            Log.w(TAG, "load ffmpeg failed")
+            e.printStackTrace()
+        }
+    }
 
 
     fun initPermissions() {
@@ -57,7 +123,7 @@ class MainActivity : AppCompatActivity() {
      * which is packaged with this application.
      */
     external fun stringFromJNI(): String
-    external fun decodeMp3():String
+
 
     companion object {
 
